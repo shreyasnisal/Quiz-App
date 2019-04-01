02704d0935aad669a694b5ebf678b5e2c70a3220
unknown <shreyasnisal@gmail.com>
2019-04-01 06:27:08 +0530

    Removed Files
    Removed actions, reducers and the redux store.

--- App.js
+++ App.js
@@ -9,8 +9,6 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
-import {Provider} from 'react-redux';
-import configureStore from './store/configure-store';
 import Home from './src/screens/Home';
 import Instructions from './src/screens/Instructions';
 import Questions from './src/screens/Questions';
@@ -30,16 +28,13 @@ const AppNavigator = createStackNavigator({
 })
 
 const AppContainer = createAppContainer(AppNavigator);
-const store = configureStore();
 
 export default class App extends Component {
   render() {
     return (
-      <Provider store={store}>
         <View style={styles.container}>
           <AppContainer />
         </View>
-      </Provider>
     );
   }
 }
